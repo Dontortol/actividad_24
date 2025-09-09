0e9e4793a7d2fe83e4047f2ef10d50f2a807d4d6
Rodrigo <rodrihercar24@gmail.com>
2025-09-09 09:26:09 -0600

    Funcion de factorial y suma

---
+++ main.py
@@ -0,0 +1,14 @@
+#Factorial de un numero
+
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def suma_naturales(n):
+    if n == 0:
+        return 0
+    else:
+        return n * suma_naturales(n-1)
+
