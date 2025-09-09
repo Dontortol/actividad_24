37d18c58be45277edce9bb922f04e8dd29be0f3b
Rodrigo <rodrihercar24@gmail.com>
2025-09-09 09:27:55 -0600

    funcion de fibonacci

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ def suma_naturales(n):
     else:
         return n * suma_naturales(n-1)
 
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
